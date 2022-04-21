ddf303f67ce5d1be1be4471be2f9834c5370f937
gerarjon <gerarsuaverdez@gmail.com>
2022-04-21 00:05:45 -0700

    added links to footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,11 +1,12 @@
-import { Link } from 'react-router-dom';
 import './style.css';
 
 const Footer = () => {
   return (
     <footer className='footer has-background-danger'>
       <div className='content has-text-centered'>
-        <p><Link to="https://github.com/gerarjon/event-booking">Github</Link></p>
+      <i className="fa-brands fa-github-alt"></i>
+        <p><a target="_blank" rel="noreferrer" href="https://github.com/gerarjon/event-booking">Github</a></p>
+        <p><a target="_blank" rel="noreferrer" href="https://github.com/gerarjon">Gerar Suaverdez ©2022</a></p>
       </div>
     </footer>
   )
